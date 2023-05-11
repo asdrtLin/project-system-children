370e1f5a3699fd0ab12d5c1c18d3a268309aef2d
c_wangkai7 <1811824503@qq.com>
2023-05-11 17:33:04 +0800

    qiankun - 子应用配置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,45 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import "./public-path";
+import { createApp, App as typeApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import routes from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import type { microProps } from "./type";
+
+let router = null;
+let instance: typeApp;
+function render(props: microProps = { container: "" }) {
+  const { container } = props;
+  router = createRouter({
+    // base: window.__POWERED_BY_QIANKUN__ ? '/vue3/' : '/',
+    history: createWebHistory(window.__POWERED_BY_QIANKUN__ ? "/vue3/" : "/"),
+    routes,
+  });
+
+  instance = createApp(App);
+  instance
+    .use(store)
+    .use(router)
+    .mount(container ? container.querySelector("#app") : "#app");
+}
+
+// 独立运行时
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap() {
+  // console.log("[vue] vue app bootstraped");
+}
+export async function mount(props: microProps) {
+  // console.log("[vue] props from main framework", props);
+  render(props);
+}
+export async function unmount() {
+  instance.unmount();
+  // instance.$el.innerHTML = "";
+  instance = null as any;
+  console.log(instance);
+  router = null;
+}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const { name } = require("./package");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    port: 7105,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  configureWebpack: {
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: "umd", // 把微应用打包成 umd 库格式
+      // jsonpFunction: `webpackJsonp_${name}`,
+    },
+  },
+});
